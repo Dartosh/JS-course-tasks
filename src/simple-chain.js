e00a500c91080dbaa178dd741cd27e13aa3fdf5a
Dartosh <dartoshka21@gmail.com>
2021-10-31 20:02:31 +0300

    refactor: add files

---
+++ src/simple-chain.js
@@ -0,0 +1,38 @@
+const { assert } = require("chai");
+const CustomError = require("../extensions/custom-error");
+
+const chainMaker = {
+
+  ans: [],
+
+  getLength() {
+    return this.ans.length;
+  },
+
+  addLink(value) {
+    this.ans.push(`( ${value} )`);
+    return this;
+  },
+
+  removeLink(position) {
+    if (this.ans[position - 1] === undefined || isNaN(position)) {
+      this.ans = [];
+      throw new Error();
+    }
+
+    this.ans.splice(position-1, 1);
+    return this;
+  },
+  
+  reverseChain() {
+    this.ans.reverse();
+    return this;
+  },
+
+  finishChain() {
+    let chain = this.ans.join('~~');
+    this.ans = [];
+    return chain;
+  }
+};
+module.exports = chainMaker;
